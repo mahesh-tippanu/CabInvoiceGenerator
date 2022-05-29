3ddc201b2bba67d4ede6961f792c3c699a50c328
mahesh-tippanu <102399228+mahesh-tippanu@users.noreply.github.com>
2022-05-29 14:13:57 +0530

    added testcase UC3_EnhancedInvoice

---
+++ InvoiceServiceTest.java
@@ -0,0 +1,43 @@
+
+import Cab_Generator.CabInvoiceGenerator;
+import Cab_Generator.InvoiceSummary;
+import Cab_Generator.Ride;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class InvoiceServiceTest {
+        CabInvoiceGenerator fare;
+
+        @BeforeEach
+        public void setUp() throws Exception {
+            fare = new CabInvoiceGenerator();
+        }
+
+        @Test
+        public void whenGivenDistanceAndTimeShouldReturnTotalFare() {
+            Assertions.assertEquals(70, fare.calculateFare(5, 20), 0.000);
+        }
+
+        @Test
+        public void whenGivenMultipleFaresShouldReturnAggregateTotalFare() {
+            Ride[] ridesArray = {new Ride(2, 20),
+                    new Ride(4, 10),
+                    new Ride(10, 30)};
+            Assertions.assertEquals(220, fare.calculateFareForMultipleRides(ridesArray), 0.0);
+        }
+
+        @Test
+        public void givenDistanceAndTimeWhenAddedShouldReturnInvoiceSummary() {
+            Ride[] rides = {
+                    new Ride(2, 5),
+                    new Ride(3, 5),
+                    new Ride(1, 1)
+            };
+            InvoiceSummary invoiceSummary = fare.invoiceSummaryCalculation(rides);
+            InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(3, 71);
+            Assertions.assertEquals(expectedInvoiceSummary, invoiceSummary);
+        }
+    }
+    
+
